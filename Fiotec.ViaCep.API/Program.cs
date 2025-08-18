24975d3f7b55cac86749f0a9c3500d3f845bd468
Lucas Pereira da Silva <lucassilva@fiotec.fiocruz.br>
2025-08-18 15:49:58 -0300

    feat(api): separação de camadas e documentação do swagger.

--- Fiotec.ViaCep.API/Program.cs
+++ Fiotec.ViaCep.API/Program.cs
@@ -1,3 +1,5 @@
+using Fiotec.ViaCep.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +8,20 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Documentação do Swagger
+builder.Services.AddSwaggerDocumentation();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Swagger no ambiente de desenvolvimento
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fiotec - ViaCEP API v1");
+        c.RoutePrefix = string.Empty; 
+    });
 }
 
 app.UseAuthorization();
